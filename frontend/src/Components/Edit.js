02dd77e79937bb94580ad9d73697fbf005f352f0
Ciara Mae Gotis <crgotis@up.edu.ph>
2017-11-27 03:06:54 +0800

    added cards to edit

--- frontend/src/Components/Edit.js
+++ frontend/src/Components/Edit.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
-
-import Button from './Button';
+import './CSS/Home.css';
+import Button from './Button.js';
+import './../../semantic/dist/semantic.min.css';
 import './../App.css';
 class Edit extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      categoriesList: []
+    }
+  }
+  componentDidMount(){
+    fetch('http://localhost:3001/quizgame/getCategories')
+    .then((response)=> {return response.json() })
+      .then((result) => {this.setState({categoriesList: result})})
+      .then(() => {console.log(this.state.categoriesList)}) 
+      .catch((e) => {console.log(e); });
+  }
   render() {
     return (
       <div id="makecenter">
-        <label id = "labelDesign"> EDIT </label> <br/>
         <br/>
+        <br/>
+        <br/>
+        <br/>
+        <label id = "labelDesign2"> EDIT </label> <br/>
+        <br/>
+        <div className = "ui cards">
+        {
+          this.state.categoriesList.map(
+                (item,index)=>{
+                  return(
+                    <div className ="card">
+                      <div className="content">
+                        <div className ="header">{item}</div>
+                        <div className ="meta">{item}</div>
+                        <div className ="description">
+                          <div className ="ui three buttons">
+                            <div className ="ui basic blue button"><i className ="unhide icon"></i></div>
+                            <div className ="ui basic green button"><i className ="edit icon"></i></div>
+                            <div className ="ui basic red button"><i className ="remove circle icon"></i></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                }
+              )
+        }
+        <div className ="card">
+                      <div className="content">
+                        <div className ="header"><button className = "ui button">+</button></div>
+                        <div className ="meta">New Category</div>
+                        <div className ="description">
+                        </div>
+                      </div>
+                    </div>
+        </div>
         <Button className="ui button" value={"Add Question"} onClick={()=>{window.location+="/add-question"}}/><br/>
         <Button className="ui button" value={"Add Category"} onClick={()=>{window.location+="/add-category"}}/><br/>
         <Button className="ui button" value={"View All Questions"} onClick={()=>{window.location+="/view-questions"}}/><br/>
@@ -16,6 +65,7 @@ class Edit extends Component {
         <Button className="ui button" value={"Edit Category"} onClick={()=>{window.location+="/edit-category"}}/><br/>
         <Button className="ui button" value={"Delete Question"} onClick={()=>{window.location+="/delete-question"}}/><br/>
 		<Button className="ui button" value={"Delete Category"} onClick={()=>{window.location+="/delete-category"}}/>
+      <button id = "menuButton" className = "massive ui inverted button" onClick = {()=>{window.location="/"}}> Back to Menu </button>
       </div>
     );
   }
