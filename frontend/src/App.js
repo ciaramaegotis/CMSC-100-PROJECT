38577ade358fe44f44ad9123c1a350d42304beed
norvylle <30821534+norvylle@users.noreply.github.com>
2017-11-26 16:26:44 +0800

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './Components/Home';
 import Play from './Components/Play';
+import AddCategory from './Components/AddCategory';
+import AddQuestion from './Components/AddQuestion';
 import Highscore from './Components/Highscore';
 import Edit from './Components/Edit';
-import Game from './Components/Game';
+import ViewQuestions from './Components/ViewQuestions';
+import './Components/CSS/Home.css';
 class App extends Component {
   render() {
     return (
@@ -15,7 +18,14 @@ class App extends Component {
         <Route exact={true} path="/play" component={Play} />
         <Route exact={true} path="/highscore" component={Highscore} />
         <Route exact={true} path="/edit" component={Edit} />
-        <Route exact={true} path="/game" component={Game} />
+        <Route exact={true} path="/edit/add-question" component={AddQuestion}/>
+        <Route exact={true} path="/edit/add-category" component={AddCategory}/>
+        <Route exact={true} path="/edit/view-questions" component={ViewQuestions}/>
+        <Route exact={true} path="/edit/view-categories" component={Home}/>
+        <Route exact={true} path="/edit/edit-question" component={Home}/>
+        <Route exact={true} path="/edit/edit-category" component={Home}/>
+        <Route exact={true} path="/edit/delete-question" component={Home}/>
+        <Route exact={true} path="/edit/delete-category" component={Home}/>
         </div>
       </Router>
       </div>
@@ -24,3 +34,9 @@ class App extends Component {
 }
 
 export default App;
+
+/*ViewCategories*/
+/*EditQuestion*/
+/*EditCategory*/
+/*DeleteQuestion*/
+/*DeleteCategory*/
