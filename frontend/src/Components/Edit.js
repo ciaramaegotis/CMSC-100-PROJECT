40dce04224256d3c3e470d585a318b63d0425679
norvylle <30821534+norvylle@users.noreply.github.com>
2017-11-26 14:56:41 +0800

    Add files via upload

--- frontend/src/Components/Edit.js
+++ frontend/src/Components/Edit.js
@@ -5,11 +5,30 @@ class Edit extends Component {
   render() {
     return (
       <div>
-        EDIT
-        <Button className="ui button" value={"AddQuestion"} onClick={()=>{window.location+="/add-question"}}/>
+        EDIT<br/>
+        <br/>
+        <Button className="ui button" value={"Add Question"} onClick={()=>{window.location+="/add-question"}}/><br/>
+        <Button className="ui button" value={"Add Category"} onClick={()=>{window.location+="/add-category"}}/><br/>
+        <Button className="ui button" value={"View All Questions"} onClick={()=>{window.location+="/view-questions"}}/><br/>
+        <Button className="ui button" value={"View All Categories"} onClick={()=>{window.location+="/view-categories"}}/><br/>
+        <Button className="ui button" value={"Edit Question"} onClick={()=>{window.location+="/edit-question"}}/><br/>
+        <Button className="ui button" value={"Edit Category"} onClick={()=>{window.location+="/edit-category"}}/><br/>
+        <Button className="ui button" value={"Delete Question"} onClick={()=>{window.location+="/delete-question"}}/><br/>
+		<Button className="ui button" value={"Delete Category"} onClick={()=>{window.location+="/delete-category"}}/>
       </div>
     );
   }
 }
 
 export default Edit;
+
+
+/*
+<div id="buttons" class="ui left labeled button" tabindex="0">
+  		<a class="ui basic right pointing label">
+    	"Category"
+    	</a>
+        <AddQuestion className="ui button" value={"+"}/>
+        </div> 
+
+*/
